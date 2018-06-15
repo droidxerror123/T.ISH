6cb9374848c651cb3715f2b4547bf56ac46307c8
Droid <39285561+droidxerror123@users.noreply.github.com>
2018-06-15 18:35:43 +0000

    Create T.ish.rb

---
+++ T.ish.rb
@@ -0,0 +1,83 @@
+puts '++++++++++++++++++++++++++++++++++'
+puts '+  Author :./Droid_X_ISH         +'
+puts '+  WA.    :+6289606402413        +'
+puts '+  Spesial ISH                   +'
+puts '+++++++++++++++++++++++++++++    +'
+puts '1. pengurus ISH             +    +'
+puts '2. All member               +    +'
+puts '3. Big Family&Brother ISH   +    +'
+puts '4. Syarat masuk             +    +'
+puts '5. Out Ya                   +    +'
+puts '++++++++++++++++++++++++++++++++++'
+puts '>>>>>>>>>>>>>>>>>>>>>'
+puts '[Droid=>> '
+a = gets.to_i
+puts '....................'
+puts ("++pengurus ISH++")
+puts ("Pengurus :.
+-./Droid_X_ISH
+-Tn.Chaplin
+-Kuro Cyber
+-Mr.END")
+a = gets.to_i
+puts '......................'
+puts ("_____All Member_____")
+puts (" all member :.
+ -./Mr.R3aD3rs
+-./Mr.CYRUS
+-./C3m#B3r00tt
+-BIaNg KEROK
+-Mr.Kovlak.JR
+-Mr.M1C1N
+-Anggrek Love Me
+-Mr.Seblak
+-Mr.IKEH
+-Al Kifly Cyber
+-ZET404
+-Mr.9UNDAM 77
+-BL4CKC4TD34T
+-Ticoalu_01
+-Mr.P3JU4N9 T3R54K1T1
+-MRX.Clay
+-FMEI13
+-Mr.DEVIL
+-Ms.Xyn
+-L4$T C0D3
+-Itsznine7
+-ItsFrogTune7
+-Mackenzie
+-Mr.OX
+-./Destro
+-JONES ")
+ a = gets.to_i
+puts '..................'
+puts ("---Big Family---")
+puts ("Anonymous :.
+-Anonymous Jember
+-Ladies Anonymous Jember
+-Indonesian Fighter Cybet
+-Indonesia Cyber Army
+-Anonymous Serang banten")
+ a = gets.to_i
+puts '...................'
+puts ("Brother ISH  :.
+-Mr.Percil.Com
+-Mr.X_XX
+-Mr.ZeroOne 01")
+ a = gets.to_i
+puts '....................'
+puts ("---Syarat Masuk---")
+puts ("Syarat :.
+-Setia
+-No Dual Team
+-Aktif
+-Menghormati Sesama Member
+-Kita Keluarga
+-Tidak Ada Kata Mastah/Noob
+Minat Chat Saya ")
+ a = gets.to_i
+puts '_____________________'
+puts ("---CROTZ---")
+puts ("Saya ./Droid_X_ISH")
+puts ("Tools Ini Khusus ISH")
+puts ("Assalamualaikum Wr.Wb")
